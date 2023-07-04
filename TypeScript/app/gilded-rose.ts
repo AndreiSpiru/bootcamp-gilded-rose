d854111fee719838dcf9dbfa5c87bac1c7d5d426
Andrei Spiru <Andrei.Spiru@softwire.com>
2023-07-04 16:59:35 +0100

    Further changes

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -19,42 +19,40 @@ export class GildedRose {
 
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      // If item is Sulfuras, nothing changes
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        let change = 0
-        if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') { // Then regular item
-          change--     
-        } else {
-            // Accounts for both Aged Brie and Backstage passes
-            change++
-            if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-              if (this.items[i].sellIn <= 5) {
-                change += 2
-              } else if (this.items[i].sellIn <= 10) {
-                change++
-              }
-            }
-      
+      if (this.items[i].name == 'Sulfuras, Hand of Ragnaros') continue
+
+      let change = 0
+      if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+        if (this.items[i].sellIn <= 0){
+          this.items[i].quality = 0
+          change = 0
+        } else if (this.items[i].sellIn <= 5) {
+          change += 3
+        } else if (this.items[i].sellIn <= 10) {
+          change +=2
+        } else{
+          change++
         }
-        
-        if (this.items[i].sellIn <= 0) {
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            this.items[i].quality = 0
-            change = 0
-          } else {
-            change *= 2
+      } else {
+          if (this.items[i].name == 'Aged Brie') { 
+            change++     
+          } else { // Then regular item
+            change--
           }
-        }
 
-        this.items[i].sellIn -= 1;
-        if (change < 0 && this.items[i].name.startsWith('Conjured')) {
-          change *=  2
-        } 
-        this.items[i].quality += change
+          if (this.items[i].sellIn <= 0) {
+           change *= 2
+          }
+     }
+      this.items[i].sellIn -= 1;
+      if (change < 0 && this.items[i].name.startsWith('Conjured')) {
+        change *=  2
+      } 
+      this.items[i].quality += change
 
-        // Quality must be between 0 and 50 if not Sulfuras
-        this.items[i].quality = clamp(this.items[i].quality, 0 , 50)
-    }
+      // Quality must be between 0 and 50 if not Sulfuras
+      this.items[i].quality = clamp(this.items[i].quality, 0 , 50)
+    
   }
   return this.items;
   }
